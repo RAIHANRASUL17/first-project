b39969dc0295fb82a192d9cf26a5626b4b2fd8e6
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-11-17 10:35:03 +0800

    first commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+// const express = require('express')
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+// const port = 3000
+
+// parsers
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+  res.send(a)
+})
+
+console.log(process.cwd())
+// D:\level-2-projects\mongoose\first-project/.env
+
+export default app
